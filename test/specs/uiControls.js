391dd52be0e72d644de765da01d581acefae8d54
Saad Selim <saadselim6@gmail.com>
2023-03-01 10:59:33 -0500

    0022

--- test/specs/uiControls.js
+++ test/specs/uiControls.js
@@ -1,62 +1,51 @@
-describe("Ecommerce Application", async () => {
-  it("Login Fail page title-Smoke", async () => {
-    //webdriverio Async  (Sync)
+const expectchai = require("chai").expect;
 
+describe("UI Controls Test Suite", async () => {
+  xit("UI Controls sanity", async () => {
     await browser.url("https://rahulshettyacademy.com/loginpagePractise/");
-    console.log(await browser.getTitle());
-    await expect(browser).toHaveTitleContaining("Rahul Shetty Academy");
-    //Css Selector, Xpath
     await $("input[name='username']").setValue("rahulshettyacademy");
-    await $("#username").setValue("secondCSS");
     const password = $("//input[@type='password']");
     await password.setValue("learning");
-    await $("#signInBtn").click();
-    await console.log(await $(".alert-danger").getText());
-    await browser.waitUntil(
-      async () => (await $("#signInBtn").getAttribute("value")) === "Sign In",
-      {
-        timeout: 5000,
-        timeoutMsg: "Error message is not showing up",
-      }
-    );
-    await console.log(await $(".alert-danger").getText());
+    //what if multiple elements $$
+    const radioButtons = await $$(".customradio");
+    const userDropdown = radioButtons[1];
+    //   await userDropdown.$("span").click(); //chaining locators-
+    //   const modal = await $(".modal-body");
+    //   await modal.waitForDisplayed();
+    //   await $("#cancelBtn").click();
+    //   console.log(await $$(".customradio")[0].$("span").isSelected());
+    //   await userDropdown.$("span").click();
+    //   await modal.waitForDisplayed();
+    //   await $("#okayBtn").click();
+    //   //validate pop up not shown up when you select admin
+    //   await $$(".customradio")[0].$("span").click();
+    //   await expect(modal).not.toBeDisplayed();
+    //   const dropdown = await $("select.form-control"); //select tag
+    //   await dropdown.selectByAttribute("value", "teach");
+    //   await dropdown.selectByVisibleText("Consultant");
+    //   await dropdown.selectByIndex(0);
+    //   console.log(await dropdown.getValue());
+    //   expectchai(await dropdown.getValue()).to.equal("stud");
+    // });
+    // xit("Dynamic Dropdown Controls Smoke", async () => {
+    //   await browser.url("/AutomationPractice/");
+    //   await $("#autocomplete").setValue("ind");
+    //   await browser.pause(3000);
+    //   let items = await $$("[class='ui-menu-item'] div");
+    //   for (var i = 0; i < (await items.length); i++) {
+    //     if ((await items[i].getText()) === "India") {
+    //       await items[i].click();
+    //       //await browser.pause(3000)
+    //     }
+    //   }
+    // });
 
-    await expect($("p")).toHaveTextContaining(
-      "username is rahulshettyacademy and Password is learning"
-    );
-  });
-
-  //   );
-  // });
-
-  // it("Login Fail page title Smoke", async () => {
-  //   await browser.url("/loginpagePractise/");
-  //   console.log(await browser.getTitle());
-
-  //   await expect(browser).toHaveTitleContaining("Rahul Shetty Academy");
-  // });
-
-  //   it("Login Success page title", async () => {
-  //     browser.url("/loginpagePractise/");
-  //     await $("input[name='username']").setValue("rahulshettyacademy");
-  //     const password = $("//input[@type='password']");
-  //     await password.setValue("learning");
-  //     await $("#signInBtn").click();
-  //     //wait until checkout button is displayed
-  //     await $(".btn-primary").waitForExist();
-  //     await expect(browser).toHaveUrlContaining("shop");
-  //     await expect(browser).toHaveTitle("ProtoCommerce");
-  //   });
-  // });
-  it("Loogin Success page title", async () => {
-    await browser.url("https://rahulshettyacademy.com/loginpagePractise/");
-    await $('input[name="username"]').setValue("rahulshettyacademy");
-    const password = $('//input[@type="password"]');
-    await password.setValue("learning");
-    await $("#signInBtn").click();
-    //wait until checkout button is displayed
-    await $(".btn-primary").waitForExist();
-    await expect(browser).toHaveUrlContaining("shop");
-    await expect(browser).toHaveTitle("ProtoCommerce");
+    // it("Checkboxes Identification", async () => {
+    //   await browser.url("/AutomationPractice/");
+    //   const element = await $$("input[type='checkbox']");
+    //   await element[1].click();
+    //   console.log(await element[1].isSelected());
+    //   console.log(await element[2].isSelected());
+    //   await browser.saveScreenshot("screenshot.png");
   });
 });
